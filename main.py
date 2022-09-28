a544e8c44a0b298fc6cc1b08593d8a466bd47940
kat/ <yuuonx@mail.ru>
2022-09-28 23:12:13 +0300

    everything fixed!

--- main.py
+++ main.py
@@ -25,19 +25,23 @@ if __name__ == '__main__':
                             print(f'/{path[:-1]}')  # вывод текущей директории
 
                     elif split_str[0] == 'ls':
+                        new_path = path
                         correct_args = True
                         incorrect_args = []
-                        if len(split_str) != 1:  # проверка на лишние аргументы. если одни пробелы - тогда все ок
-                            for i in range(1, len(split_str)):
-                                if split_str[i] != '':
-                                    incorrect_args.append(split_str[i])
-                            if len(incorrect_args) != 0:
-                                correct_args = False
+                        if len(split_str) != 1:
+                            new_path = f'{split_str[1]}/'
+                            if not new_path in all_folders:
+                                for i in range(1,
+                                               len(split_str)):  # проверка на лишние аргументы. если одни пробелы - тогда все ок
+                                    if split_str[i] != '':
+                                        incorrect_args.append(split_str[i])
+                                if len(incorrect_args) != 0:
+                                    correct_args = False
                         if correct_args:
                             unic_list = set()  # делаем множество из папок
                             for folder in all_folders:  # перебираем все папки
-                                if folder.startswith(path):  # смотрим что у каждой внутри
-                                    folder = folder[len(path):]  # обрезаем название папки из пути
+                                if folder.startswith(new_path):  # смотрим что у каждой внутри
+                                    folder = folder[len(new_path):]  # обрезаем название папки из пути
                                     unic_list.add(
                                         (folder.split('/')[0]))
                             # выводим всё, кроме пустых строк
@@ -68,26 +72,32 @@ if __name__ == '__main__':
                             if command_str[0] == '/':  # если абсолютный путь
                                 tmp_dir = f'{command_str[1:]}/'
                             else:
-                                tmp_dir = f'{path}{command_str}/' # если обычный
+                                tmp_dir = f'{path}{command_str}/'  # если обычный
                             if tmp_dir in all_folders:  # если данная dir существует в листе всех папок
                                 path = tmp_dir  # преобразование временной dir в текущую
                             elif tmp_dir[:-1] in all_folders:
-                                print(f'sh: cd: can\'t cd to {command_str}: Not a directory') # если файл
+                                print(f'sh: cd: can\'t cd to {command_str}: Not a directory')  # если файл
                             else:
                                 print(f'sh: cd: can\'t cd to {command_str}: No such file or directory')
 
                     elif split_str[0] == 'cat':
-                        f = ' '.join(split_str[1:])  # соединение аргумента через пробелы
-                        with zipfile.open(f'{path}{f}', 'r') as file:  # открытие файла из архива
-                            for a in file.readlines():
-                                for b in a.decode('utf-8').strip():
-                                    print(b, end='')  # вывод текста из файла
-                                print()
+                        file_path = f'{split_str[1]}/'
+                        if file_path in all_folders:
+                            with zipfile.open(f'{path}{file_path[:-1]}', 'r') as file:  # открытие файла из архива
+                                for a in file.readlines():
+                                    for b in a.decode('utf-8').strip():
+                                        print(b, end='')  # вывод текста из файла
+                                    print()
+                        else:
+                            print(f"cat: can\'t open \'{file_path[:-1]}\': No such file or directory")
+
                     elif split_str[0] == 'exit':
                         break
+
                     else:
                         print(f'sh: {split_str[0]}: command not found')
         else:
             print('Enter filename')
+
     except Exception as e:
         print(e)
