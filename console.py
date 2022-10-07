4853f7e94dfe11b83559e7a7a16a554a5fc2beb5
kat/ <yuuonx@mail.ru>
2022-10-07 09:28:37 +0300

    perfect class

--- console.py
+++ console.py
@@ -8,24 +8,46 @@ class Console:
         self.directories = directories
         self.zipfile = zipfile
 
+    def start(self):
+        if self.current_path == '':
+            print("[~vshell]:/ ", end='')
+        else:
+            print(f"[~vshell]:/{self.current_path[:-1]} ", end='')
+        users_input = input()
+        self.users_input = users_input.split(' ')
+
+        if self.users_input[0] == "pwd":
+            self.pwd_command()
+
+        elif self.users_input[0] == "ls":
+            self.ls_command()
+
+        elif self.users_input[0] == "cd":
+            self.cd_command()
+
+        elif self.users_input[0] == "cat":
+            self.cat_command()
+        else:
+            print(f"vsh: {self.users_input[0]}: command not found")
+
     def pwd_command(self):
         if self.current_path == '':
             print('/')
         else:
             print(f"/{self.current_path[:-1]}")
 
-    def ls_command(self, users_input):
+    def ls_command(self):
         path = self.current_path
         normal_input = True
         incorrect_input_cases = []
-        if len(users_input) != 1:
-            path = f'{users_input[1]}/'
+        if len(self.users_input) != 1:
+            path = f'{self.users_input[1]}/'
             if path[0] == '/':
                 path = path[1:]
             if not path in self.directories and path != '/':
-                for i in range(1, len(users_input)):
-                    if users_input[i] != '':
-                        incorrect_input_cases.append(users_input[i])
+                for i in range(1, len(self.users_input)):
+                    if self.users_input[i] != '':
+                        incorrect_input_cases.append(self.users_input[i])
                 if len(incorrect_input_cases) != 0:
                     normal_input = False
         if path == '/':  # if root
@@ -47,9 +69,9 @@ class Console:
             for bad in incorrect_input_cases:
                 print("ls: " + bad + ": No such file or directory")
 
-    def cd_command(self, users_input):
+    def cd_command(self):
         input = ''
-        for inp in users_input[1:]:
+        for inp in self.users_input[1:]:
             if inp != '':
                 input = inp
                 break
@@ -77,16 +99,16 @@ class Console:
             else:
                 print(f"sh: cd: can\'t cd to {input}: No such file or directory")
 
-    def cat_command(self, users_input):
+    def cat_command(self):
         all_files = []
-        for file in users_input[1:]:
+        for file in self.users_input[1:]:
             if file != '':
                 all_files.append(file)
         if len(all_files) > 0:
             for file_for_reading in all_files:
                 if file_for_reading[0] == '/':
                     file_for_reading = file_for_reading[1:]
-                if file_for_reading in self.directories:
+                if f"{self.current_path}{file_for_reading}" in self.directories:
                     with self.zipfile.open(f"{self.current_path}{file_for_reading}", 'r') as file:
                         for line in file.readlines():
                             for symbol in line.decode('utf-8').strip():
--- main.py
+++ main.py
@@ -6,33 +6,11 @@ from console import Console
 
 if __name__ == '__main__':
     try:
-        if len(sys.argv) > 1:
-            with ZipFile(sys.argv[1]) as zipfile:
-                directories = zipfile.namelist()
-                vshell = Console(directories, zipfile)
-                while True:
-                    if vshell.current_path == '':
-                        print("[~vshell]:/ ", end='')
-                    else:
-                        print(f"[~vshell]:/{vshell.current_path[:-1]} ", end='')
-                    users_input = input()
-                    users_input = users_input.split(' ')
-
-                    if users_input[0] == "pwd":
-                        vshell.pwd_command()
-
-                    elif users_input[0] == "ls":
-                        vshell.ls_command(users_input)
-
-                    elif users_input[0] == "cd":
-                        vshell.cd_command(users_input)
-
-                    elif users_input[0] == "cat":
-                        vshell.cat_command(users_input)
-                    else:
-                        print(f"vsh: {users_input[0]}: command not found")
-        else:
-            print("Enter filename")
+        with ZipFile(sys.argv[1]) as zipfile:
+            directories = zipfile.namelist()
+            vshell = Console(directories, zipfile)
+            while True:
+                vshell.start()
 
     except Exception as exception:
         print(exception)
